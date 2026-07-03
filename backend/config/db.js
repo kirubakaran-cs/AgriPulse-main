6a27ac3f80a272b9b2d18ef8a620573d02bb9d8a
Kirubakaran <vkirubakaran633@gmail.com>
2026-07-03 11:14:49 +0530

    Use environment variables

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,20 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "agripulse",
-    connectTimeout: 10000
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  connectTimeout: 10000,
 });
 
 db.connect((err) => {
-    if (err) {
-        console.error("Connection Failed:");
-        console.error(err);
-        return;
-    }
-    console.log("✅ MySQL Connected Successfully");
+  if (err) {
+    console.error("Connection Failed:", err);
+    return;
+  }
+  console.log("✅ MySQL Connected Successfully");
 });
 
 module.exports = db;
